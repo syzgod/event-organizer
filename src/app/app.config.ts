5c777397dfaf99e95fa2b5edb628020344146219
Viktor Berczeli <viktor.berczeli@gmail.com>
2026-02-08 22:04:51 +0100

    Add PrimeNG and related dependencies; configure theme settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+     providePrimeNG({
+      ripple: true,
+            theme: {
+                preset: Aura,
+                options: {
+    darkModeSelector: '.my-app-dark'
+}
+            }
+          })
   ]
 };
